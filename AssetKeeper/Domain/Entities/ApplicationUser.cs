6ba65999066dde28599dba7340b79e4ba826384b
wreza1987 <wreza1987@gmail.com>
2026-06-29 11:06:21 +0330

    Update .gitignore

--- AssetKeeper/Domain/Entities/ApplicationUser.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Identity;
-
-namespace AssetKeeper.Domain.Entities;
-
-public class ApplicationUser : IdentityUser
-{
-    public string? PersonnelCode { get; set; }
-    public int? EmployeeId { get; set; }
-}
